21c0a870fb76b5ad58f8b76f54b85feea529d541
canyie <a1364259@163.com>
2022-06-26 20:44:19 +0800

    Move FAB up when SnackBar is shown

--- app/src/main/java/com/topjohnwu/magisk/ui/log/LogFragment.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/log/LogFragment.kt
@@ -20,10 +20,9 @@ class LogFragment : BaseFragment<FragmentLogMd2Binding>() {
 
     override val layoutRes = R.layout.fragment_log_md2
     override val viewModel by viewModel<LogViewModel>()
-    override val snackbarView: View?
+    override val snackbarView: View
         get() = if (isMagiskLogVisible) binding.logFilterSuperuser.snackbarContainer
-                else super.snackbarView
-    override val snackbarAnchorView get() = binding.logFilterToggle
+                else binding.logFilterToggle
 
     private var actionSave: MenuItem? = null
     private var isMagiskLogVisible
